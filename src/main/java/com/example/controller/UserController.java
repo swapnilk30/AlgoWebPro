3171117cbc99a2a591f767032811379ee7eda448
swapnilk30 <sk9137587676@gmail.com>
2024-10-10 20:30:05 +0530

    test controller

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome To AlgoWebPro!!";
+	}
 }
